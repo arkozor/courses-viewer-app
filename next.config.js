6855b08e11dfea59660f5faa4052297054d5eec9
Jan-Vermeulen <jan.vermeulen@ricardo.ch>
2020-11-13 22:01:07 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  env: {
-    APP_ENV: 'local',
-    FLAGSMITH_ENV_KEY:'Jy7QznRFqoWmnBDW47Pi8c'
-  },
+    env: {
+        APP_ENV: 'local',
+        FLAGSMITH_ENV_KEY: 'Jy7QznRFqoWmnBDW47Pi8c'
+    }
 }
