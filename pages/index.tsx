7257c4c356095adafe803ef52b816b411c09fe31
Vincent Gilabert <vincent.gilabert@gmail.com>
2021-02-05 17:44:43 +0100

    Add chips component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import ChipFilters from 'components/ChipFilters'
+
 
 const HomePage = () => {
-    return <div>Hello World</div>
+    return (
+        <div>
+            <ChipFilters/>
+            <div>Hello World</div>
+        </div>
+    )
 }
 
 export default HomePage
