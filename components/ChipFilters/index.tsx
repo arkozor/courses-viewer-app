1db5f55533c152b2e6392ff11f6acea45f124516
Vincent Gilabert <vincent.gilabert@gmail.com>
2021-03-04 14:48:50 +0100

    Run pretty

--- components/ChipFilters/index.tsx
+++ components/ChipFilters/index.tsx
@@ -4,22 +4,38 @@ import classes from './style.module.scss'
 import classnames from 'classnames'
 
 const ChipFilters = (): JSX.Element => {
-    const [isSelected, setIsSelected] = React.useState("")
+    const [isSelected, setIsSelected] = React.useState('')
     const chips = [
-        {className: classes.cSharpChip, label: "CSharp"},
-        {className: classes.javaChip, label: "Java"},
-        {className: classes.jsChip, label: "JavaScript"},
-        {className: classes.phpChip, label: "Php"},
+        { className: classes.cSharpChip, label: 'CSharp' },
+        { className: classes.javaChip, label: 'Java' },
+        { className: classes.jsChip, label: 'JavaScript' },
+        { className: classes.phpChip, label: 'Php' }
     ]
 
     function resolveClick(event: React.MouseEvent<HTMLElement>) {
-        const {innerText} = event.target as HTMLElement
+        const { innerText } = event.target as HTMLElement
         setIsSelected(innerText)
     }
 
     return (
         <div>
-            {chips.map((chip) => (<Chip key={chip.label} classes={{root: classnames(chip.className, classes.chip, classes.selected && isSelected===chip.label?classes.selected:"")}} label={chip.label} clickable onClick={resolveClick}/>))}
+            {chips.map((chip) => (
+                <Chip
+                    key={chip.label}
+                    classes={{
+                        root: classnames(
+                            chip.className,
+                            classes.chip,
+                            classes.selected && isSelected === chip.label
+                                ? classes.selected
+                                : ''
+                        )
+                    }}
+                    label={chip.label}
+                    clickable
+                    onClick={resolveClick}
+                />
+            ))}
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ChipFilters from 'components/ChipFilters'
 
-
 const HomePage = () => {
     return (
         <div>
-            <ChipFilters/>
+            <ChipFilters />
             <div>Hello World</div>
         </div>
     )
