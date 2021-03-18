9020a2e6c7fb831faee6cdaf3891335116c5496f
Vincent Gilabert <vincent.gilabert@gmail.com>
2021-03-18 17:47:24 +0100

    Remove popover

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -5,35 +5,8 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import classes from './style.module.scss'
-import Popover from '@material-ui/core/Popover';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        popover: {
-            pointerEvents: 'none',
-            width: '320px'
-        },
-        paper: {
-            padding: theme.spacing(1),
-        },
-    }),
-);
 
 const CourseCard = (props): JSX.Element => {
-    const MUIclasses = useStyles();
-    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
-
-    const handlePopoverOpen = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handlePopoverClose = () => {
-        setAnchorEl(null);
-    };
-
-    const open = Boolean(anchorEl);
-
     return (
         <div className={classes.card}>
             <Card>
@@ -42,11 +15,7 @@ const CourseCard = (props): JSX.Element => {
                         component="img"
                         image="/images/JDG.png"
                         height="180"
-                        title="Preview"
-                        aria-owns={open ? 'mouse-over-popover' : undefined}
-                        aria-haspopup="true"
-                        onMouseEnter={handlePopoverOpen}
-                        onMouseLeave={handlePopoverClose}
+                        title="Vignette"
                     />
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="h2">
@@ -58,32 +27,6 @@ const CourseCard = (props): JSX.Element => {
                     </CardContent>
                 </CardActionArea>
             </Card>
-            <Popover
-                id="mouse-over-popover"
-                className={MUIclasses.popover}
-                classes={{
-                    paper: MUIclasses.paper,
-                }}
-                open={open}
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                }}
-                onClose={handlePopoverClose}
-                disableRestoreFocus
-            >
-                 <Typography gutterBottom variant="h5" component="h2">
-                    {props.name}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    {props.description}
-                </Typography>
-            </Popover>
         </div>
     );
 }
