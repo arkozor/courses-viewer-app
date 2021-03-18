1090e1517eadbbdbc198fc51dbb649be947b9d3e
tanguy <2014194@campusid.eu>
2021-03-18 18:03:08 +0100

    slider

---
+++ components/CarouselView/index.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import Carousel from 'react-elastic-carousel'
+import classes from './style.module.scss'
+
+const courses = [
+    {
+        id: 1,
+        title: 'card'
+    },
+    {
+        id: 2,
+        title: 'card'
+    },
+    {
+        id: 3,
+        title: 'card'
+    },
+    {
+        id: 4,
+        title: 'card'
+    },
+    {
+        id: 5,
+        title: 'card'
+    },
+    {
+        id: 6,
+        title: 'card'
+    },
+    {
+        id: 7,
+        title: 'card'
+    },
+    {
+        id: 8,
+        title: 'card'
+    },
+    {
+        id: 9,
+        title: 'card'
+    },
+    {
+        id: 10,
+        title: 'card'
+    },
+    {
+        id: 11,
+        title: 'card'
+    },
+    {
+        id: 11,
+        title: 'card'
+    }
+]
+
+const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 5 }
+]
+
+const SlideShow = () => {
+    return (
+        <div className={classes.app}>
+            <Carousel breakPoints={breakPoints} className={classes.carousel}>
+                {courses.map((course) => (
+                    <div key={course.id} className={classes.carouselItem}>
+                        <div className={classes.card}>
+                            <h2>{course.title}</h2>
+                        </div>
+                    </div>
+                ))}
+            </Carousel>
+        </div>
+    )
+}
+
+export default SlideShow
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import Card from 'components/Card'
 import React from 'react'
 import ChipFilters from '../components/ChipFilters'
+import Carousel from '../components/CarouselView'
 
 const HomePage = (): JSX.Element => {
     return (
         <div>
             <ChipFilters />
-            <Card name={"Java"}  description={"Ceci est un super cours de Java."}/>
+            <Card
+                name={'Java'}
+                description={'Ceci est un super cours de Java.'}
+            />
+            <Carousel />
             <div>Hello World</div>
         </div>
     )
