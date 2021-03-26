c7c1ec89ad2c0739fb5f175eaa3d52c11fa9eed7
Vincent Gilabert <vincent.gilabert@gmail.com>
2021-03-26 16:46:45 +0100

    Title component

---
+++ pages/course/index.tsx
@@ -0,0 +1,11 @@
+import CourseTitle from 'components/CourseTitle'
+import React from 'react'
+import classes from './style.module.scss'
+
+const Course = (): JSX.Element => {
+    return (
+        <CourseTitle text="CSharp - les bases"></CourseTitle>
+    )
+}
+
+export default Course
